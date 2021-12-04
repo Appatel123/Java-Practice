06e772bfa85dc94efddc4277fc9177f973f26935
amit.patel536@gmail.com <Appatel123>
2021-12-04 12:21:44 +0000

    Today practice about git

---
+++ src/class7/GitPractice.java
@@ -0,0 +1,9 @@
+package class7;
+
+public class GitPractice {
+    //do not work on the main/dev/master
+    //Create a new branch from main
+    //Switch to your local branch
+    //And start working(in local) once work is done you can merge local branch into main
+
+}
